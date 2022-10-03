ff439d30071595b72312b4c06ef53b300967d9a5
erzaffasya <effasya@gmail.com>
2022-10-03 12:24:44 +0800

    setting gcalendar

--- database/migrations/2022_09_29_143213_create_pengunjung_table.php
+++ database/migrations/2022_09_29_143213_create_pengunjung_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('pengunjung', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("user_id")->nullable()->constrained("users")->onDelete("cascade")->onUpdate("cascade");
             $table->timestamps();
         });
     }
